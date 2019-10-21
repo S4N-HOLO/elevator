8f32498c5baf678dfcc43838be51db0531221607
Alexandr Shcherbakov <sc.fatman@gmail.com>
2019-10-21 10:30:54 +0300

    dfgfgdfdfdfgdfg

--- elevator ver 1/Form1.cs
+++ elevator ver 1/Form1.cs
@@ -18,19 +18,33 @@ namespace elevator_ver_1
         private int level_now = 1;
 
         private int i = 0;
-        private int d = 0;
         public Form1()
         {
 
             InitializeComponent();
         }
+        private void doors_close()
+        {
+            while (panel1.Location.X != 45)
+            {
+                System.Threading.Thread.Sleep(50);
+                panel1.Location = new Point(panel1.Location.X - 1, panel1.Location.Y);
+                panel3.Location = new Point(panel3.Location.X + 1, panel3.Location.Y);
+            }
+        } //закрытие дверей
+
+        private void doors_open()
+        {
+            while (panel1.Location.X != 85)
+            {
+                System.Threading.Thread.Sleep(50);
+                panel1.Location = new Point(panel1.Location.X + 1, panel1.Location.Y);
+                panel3.Location = new Point(panel3.Location.X - 1, panel3.Location.Y);
+            }
+        } //открытие дверей
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //    нажали кнопку этажа и показали картинку этажа, затем указакли в level_dest данный этаж из кнопки, запустили
-            //        функцию с значением переменной конкретного этажа из предыдущего нажатия( по умолч. 1) и текущего level_dest
-
-          //  this.pictureBox1.Image = Image.FromFile("C:\\Users\\asherbakov\\Desktop\\elevator-master\\elevator ver 1\\elevator_1stfloor.jpg");
 
             level_dest = 1;
             moving_elevator(level_dest, level_now);
@@ -40,11 +54,6 @@ namespace elevator_ver_1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //    нажали кнопку этажа и показали картинку этажа, затем указакли в level_dest данный этаж из кнопки, запустили
-            //        функцию с значением переменной конкретного этажа из предыдущего нажатия( по умолч. 1) и текущего level_dest
-
-            //this.pictureBox1.Image = Image.FromFile("C:\\Users\\asherbakov\\Desktop\\elevator-master\\elevator ver 1\\elevator_2ndfloor.jpg");
-
             level_dest = 2;
             moving_elevator(level_dest, level_now);
             level_now = 2;
@@ -53,11 +62,6 @@ namespace elevator_ver_1
 
         private void button3_Click(object sender, EventArgs e)
         {
-        //    нажали кнопку этажа и показали картинку этажа, затем указакли в level_dest данный этаж из кнопки, запустили
-        //        функцию с значением переменной конкретного этажа из предыдущего нажатия( по умолч. 1) и текущего level_dest
-            
-          //  this.pictureBox1.Image = Image.FromFile("C:\\Users\\asherbakov\\Desktop\\elevator-master\\elevator ver 1\\elevator_3rdfloor.jpg");
-           
             level_dest = 3;
             moving_elevator(level_dest, level_now);
             level_now = 3;
@@ -125,32 +129,26 @@ namespace elevator_ver_1
 
         private void timer1_Tick(object sender, EventArgs e) // понижение этажа на -1 уровень
         {
+            doors_close();
             panel2.Location = new Point(panel2.Location.X, panel2.Location.Y + 1); // поменять на 1
             i = i + 1;
             if (i == 112) //поменять на ==
             {
                 i = 0;
+                doors_open();
                 timer1.Enabled = false;
             }
          }
 
-
         private void timer2_Tick(object sender, EventArgs e) // повышение этажа на +1 уровень
         {
-            
+            doors_close();
             panel2.Location = new Point(panel2.Location.X, panel2.Location.Y - 1); // поменять на 1
             i = i + 1;
             if (i == 112)
             {
                 i = 0;
-                
-                while (d != 50)
-                {
-                    panel1.Location = new Point(panel1.Location.X + 1, panel1.Location.Y);
-                    d = d + 1;
-                }
-
-                d = 0;
+                doors_open();
                    
                 timer2.Enabled = false;
             }
@@ -159,11 +157,13 @@ namespace elevator_ver_1
 
         private void timer3_Tick(object sender, EventArgs e) // +2 уровня
         {
+            doors_close();
             panel2.Location = new System.Drawing.Point(panel2.Location.X, panel2.Location.Y - 1); // поменять на 1
             i = i + 1;
             if (i == 224)
             {
                 i = 0;
+                doors_open();
                 timer3.Enabled = false;
 
             }
@@ -171,11 +171,13 @@ namespace elevator_ver_1
 
         private void timer4_Tick(object sender, EventArgs e)  //-2 уровня
         {
+            doors_close();
             panel2.Location = new System.Drawing.Point(panel2.Location.X, panel2.Location.Y + 1); // поменять на 1
             i = i + 1;
             if (i == 224)
             {
                 i = 0;
+                doors_open();
                 timer4.Enabled = false;
             }
         }
